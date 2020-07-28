46973aec664a3044c48cf5022e0e38878358156c
adip1343 <aadhip43@gmail.com>
2020-07-28 13:12:53 +0530

    Updates

---
+++ precache-manifest.10287b808d1a6adbab866c106c8c6be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435056d21048e92fb2fd378b518b3bc0",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "e39c9558a95e06c3ddd3",
+    "url": "/weather/static/css/main.e41c332e.chunk.css"
+  },
+  {
+    "revision": "4f8dfb2d777aa8537d86",
+    "url": "/weather/static/js/2.18f5d576.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.18f5d576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39c9558a95e06c3ddd3",
+    "url": "/weather/static/js/main.64b4cfa2.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
